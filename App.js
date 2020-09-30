e6ba8d75329cc8b7c71fe46b04a882bc95015673
alestor123 <alestoraldous@gmail.com>
2020-09-30 15:53:46 +0530

    added main code

--- App.js
+++ App.js
@@ -1,2 +1,23 @@
 #!/usr/bin/env node
 var { spawn } = require('child_process')
+if (!process.argv[2]) {
+    console.log('Usage: $ command [args] where command is any valid terminal command.')
+    process.exit()
+}
+
+// Run a command with its arguments by spawning a process and
+// transparently routing stdio and signals through this parent process
+const spawnedProcess = spawn(
+    process.argv[2],
+    process.argv.slice(3),
+    {stdio: 'inherit'}
+);
+
+// Route signals through so they still reach the spawned process
+process.on('SIGTERM', () => spawnedProcess.kill('SIGTERM'))
+process.on('SIGINT', () => spawnedProcess.kill('SIGINT'))
+process.on('SIGBREAK', () => spawnedProcess.kill('SIGBREAK'))
+process.on('SIGHUP', () => spawnedProcess.kill('SIGHUP'))
+
+// Exit this process when the spawned process exits
+spawnedProcess.on('exit', process.exit)
